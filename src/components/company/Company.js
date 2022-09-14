4c24b8429c72fa9e6b72585ff691867e8eb89348
ranganathnk <ranganathnkulkarni@gmail.com>
2022-09-14 17:41:08 +0530

    testing with onchange and placeholder

--- src/components/company/Company.js
+++ src/components/company/Company.js
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const Company = ({name}) => {
+const Company = ({ name }) => {
+  const [country, setCountry] = useState("India");
+
+  const countryChangeHandler = (event) => {
+    console.log(event);
+    setCountry(event.target.value);
+  };
+
   return (
     <div>
-        <h2>Company | Testing props, Events, States, Hooks, Props, CSS, JSX</h2>
-        <h3 data-testid="companyName">cognizant's official website</h3>
-        <h4 data-testid="companyFullName">Company name : {name}</h4>
-    </div>
-  )
-}
+      <h2>
+        Company | Testing props, Events, States, Hooks, Props, CSS, JSX,
+        Snapshot
+      </h2>
+      <h3 data-testid="companyName">cognizant's official website</h3>
+      <h4 data-testid="companyFullName">Company name : {name}</h4>
 
-export default Company
+      <br />
+      {/* Working with onchange */}
+      <label>Enter your country :</label>
+      <input
+        type="text"
+        placeholder="Enter Country"
+        value={country}
+        onChange={countryChangeHandler}
+      />
+      <p>Head over to Cognizant {country} website</p>
+    </div>
+  );
+};
+
+export default Company;
--- src/components/company/Company.test.js
+++ src/components/company/Company.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Company from "./Company";
 
 describe("Company component", () => {
@@ -9,8 +9,41 @@ describe("Company component", () => {
   });
 
   //Props testing
-  it("receives ", () => {
+  it("receives name prop", () => {
     render(<Company name="Cognizant Technology Solutions" />);
-    expect(screen.getByTestId("companyFullName")).toHaveTextContent("Cognizant Technology Solutions");
+    expect(screen.getByTestId("companyFullName")).toHaveTextContent(
+      "Cognizant Technology Solutions"
+    );
+  });
+
+  //input element with 'Enter country' as placeholder
+  it(`has an input element with 'Enter country' as placeholder`, () => {
+    render(<Company />);
+    const country = screen.getByPlaceholderText("Enter Country");
+    expect(country).toBeTruthy();
+  });
+
+  //useState, onChange testing
+  it("should update country onChange event", () => {
+    render(<Company />);
+    // let's find the input element that has onChange handler
+    const countryInput = screen.getByPlaceholderText("Enter Country");
+
+    // now checking if onChange event is working well or not
+    // mock fire the onChange event with mock value
+
+    fireEvent.change(countryInput, {
+      target: {
+        value: "Australia",
+      },
+    });
+    expect(countryInput.value).toBe("Australia");
+
+    fireEvent.change(countryInput, {
+      target: {
+        value: "UK",
+      },
+    });
+    expect(countryInput.value).toBe("UK");
   });
 });
